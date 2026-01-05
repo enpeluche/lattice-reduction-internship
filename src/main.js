13af3c0ca623ae65d2cd2fcd812e44ef932e7485
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-05 01:41:31 +0100

    fix(imports): use relative paths to fix GitHub Pages loading

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { loadTableOfContents } from "/src/components/TableOfContents/TableOfContents.js";
-import { createNews } from "/src/components/News/News.js";
-import { createProject } from "/src/components/Project/Project.js";
-import { makeArticle } from "/src/components/Course/Course.js";
-import { loadFooter } from "/src/components/Footer/footer.js";
-import { setupKatex } from "/src/katex-config.js";
+import { loadTableOfContents } from "./src/components/TableOfContents/TableOfContents.js";
+import { createNews } from "./src/components/News/News.js";
+import { createProject } from "./src/components/Project/Project.js";
+import { makeArticle } from "./src/components/Course/Course.js";
+import { loadFooter } from "./src/components/Footer/footer.js";
+import { setupKatex } from "./src/katex-config.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   loadTableOfContents();
